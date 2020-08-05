18c8ca3f6e9e8db3ff89d16cc6eb1f3236d0bb3b
Sergio Fernandez <sfernandez@mindden.com>
2020-08-05 15:44:47 +0200

    Set score on init database

--- backend/scripts/initDb.js
+++ backend/scripts/initDb.js
@@ -3,16 +3,23 @@ const ImageModel = require("../src/modules/images/persistence/ImageModel");
 const ImageSequenceModel = require("../src/modules/images/persistence/ImageSequenceModel");
 const AdModel = require("../src/modules/ads/persistence/AdModel");
 const AdSequenceModel = require("../src/modules/ads/persistence/AdSequenceModel");
+const { fromPayloadToAd } = require("../src/modules/ads/mapper/AdMapper");
+const { calculateScore } = require("../src/modules/ads/service/ScoreCalculator");
 
 const images = JSON.parse(fs.readFileSync("./scripts/data/images.json"));
-const ads = JSON.parse(fs.readFileSync("./scripts/data/ads.json"));
+let ads = JSON.parse(fs.readFileSync("./scripts/data/ads.json"));
 
 const insertData = async () => {
   try {
+
     await ImageModel.insertMany(images);
     await new ImageSequenceModel({ id: 7 }).save();
+
+    ads = ads.map(ad => fromPayloadToAd(ad)).map(ad => ({ ...ad, score: calculateScore(ad) }));
+
     await AdModel.insertMany(ads);
     await new AdSequenceModel({ id: 11 }).save();
+
     console.log("Init DB ok");
   } catch (e) {
     console.log(e);
